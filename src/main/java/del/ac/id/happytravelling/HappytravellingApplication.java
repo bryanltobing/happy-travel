d915977d2aa3d03064e6096bb19fc4b2f6df974f
bryantobing12 <bryantobing0@gmail.com>
2020-10-04 15:15:20 +0700

    Initial commit

---
+++ src/main/java/del/ac/id/happytravelling/HappytravellingApplication.java
@@ -0,0 +1,13 @@
+package del.ac.id.happytravelling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappytravellingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappytravellingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/del/ac/id/happytravelling/HappytravellingApplicationTests.java
@@ -0,0 +1,13 @@
+package del.ac.id.happytravelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappytravellingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
